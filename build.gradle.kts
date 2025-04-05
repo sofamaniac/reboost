ac11b60068a6ead3bb484cb02ee55ed17373fc29
Antoine Grimod <antoine.grimod@gmail.com>
2025-04-05 14:07:16 +0200

    Update to build gradle and add openapi generator plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,12 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+    id("org.openapi.generator") version "7.12.0" apply false
 }
+
+
+tasks.updateDaemonJvm {
+    jvmVersion = JavaLanguageVersion.of(21)
+}
+
+
